094156ca9e47de3715e0dca183ac37f43d106b89
Wiktor <wiktor.tatarynowicz@gmail.com>
2023-07-31 18:57:46 +0200

    add projects 1.1

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ let blur_p1 =document.querySelector(".project1 a div");
 let blur_element2 = document.querySelector(".project2 a");
 let blur_p2 =document.querySelector(".project2 a div");
 blur_element1.addEventListener("mouseover",()=>{
-    blur_p1.classList.add("blur_p");
     blur_p1.style.display="flex";
 })
 blur_element1.addEventListener("mouseout",()=>{
-    blur_p1.classList.remove("blur_p");
     blur_p1.style.display="none";
 })
 blur_element2.addEventListener("mouseover",()=>{
-    blur_p2.classList.add("blur_p");
     blur_p2.style.display="flex";
 })
 blur_element2.addEventListener("mouseout",()=>{
