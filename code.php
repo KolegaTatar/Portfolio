3adecae184f918d94eb80d178e1a3d010ea02dcf
Wiktor <wiktor.tatarynowicz@gmail.com>
2024-05-06 21:12:49 +0200

    create cookies

--- code.php
+++ code.php
@@ -5,5 +5,15 @@ $email = $_POST['email'];
 $text = $_POST['subject'];
 $message = $_POST['message'];
 
-*/
 
+*/
+#
+
+setcookie("New_Cookies",1,time()*3600);
+
+if(!isset($_COOKIE["New_Cookies"])){
+    echo "Witaj na stronie";
+}
+else{
+    echo "Miło Cię widzieć ponownie";
+};
